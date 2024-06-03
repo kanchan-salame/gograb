a77e914f4e7cec3622945ac1278bb2e285c4b908
kanchan-salame <hemantsalame54@gmail.com>
2024-06-03 23:16:48 +0530

    Added dark light theme switcher

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,22 +1,26 @@
 <script setup>
-import AppLayout from '@/Layouts/AppLayout.vue';
-import Welcome from '@/Components/Welcome.vue';
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Welcome from "@/Components/Welcome.vue";
+import { Head, Link } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <AppLayout title="Dashboard">
-        <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                Dashboard
-            </h2>
-        </template>
+  <Head title="Dashboard" />
+  <AppLayout title="Dashboard">
+    <template #header>
+      <h2
+        class="dark:text-white font-semibold text-xl text-gray-800 leading-tight"
+      >
+        Dashboard
+      </h2>
+    </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Welcome />
-                </div>
-            </div>
+    <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+          <Welcome />
         </div>
-    </AppLayout>
+      </div>
+    </div>
+  </AppLayout>
 </template>
