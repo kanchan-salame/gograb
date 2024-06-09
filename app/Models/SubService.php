03c914d567fb0588fd4654af087158630d9081a8
kanchan-salame <hemantsalame54@gmail.com>
2024-06-09 07:39:54 +0530

    added models

---
+++ app/Models/SubService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubService extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'service_id',
+        'image',
+    ];
+
+    protected $appends = ['imagepath'];
+
+    public function getImagePathAttribute()
+    {
+        return url('storage/'.$this->image);
+    }
+}
