f4614065ab1c6f7830c25ba6d775ec472df81e84
kanchan-salame <hemantsalame54@gmail.com>
2024-06-13 08:59:16 +0530

    product and categories models

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProductCategory extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'image',
+    ];
+
+    protected $appends = ['imagepath'];
+
+    public function getImagePathAttribute()
+    {
+        return url('storage/'.$this->image);
+    }
+}
