c1040604d3bc32f9365ba76e2cbdf476a00b2b14
kanchan-salame <hemantsalame54@gmail.com>
2024-06-13 09:01:51 +0530

    Product and categories controllers

---
+++ app/Http/Controllers/ProductSubCategoryController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductSubCategory;
+use Illuminate\Http\Request;
+
+class ProductSubCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductSubCategory $productSubCategory)
+    {
+        //
+    }
+}
