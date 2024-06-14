b269e098d4b8c3758f5506876090ee8cde9ba57a
kanchan-salame <hemantsalame54@gmail.com>
2024-06-14 14:35:14 +0530

    cart and food order models

---
+++ app/Models/Cart.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'restaurant_menu_item_id',
+        'package_size_id',
+        'quantity',
+    ];
+
+
+
+}
