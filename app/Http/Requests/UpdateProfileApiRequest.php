61bc1aaee85a6a81932e1a1a2a88ffb7651630b8
kanchan-salame <hemantsalame54@gmail.com>
2024-07-01 04:33:45 +0530

    added columns to users table and change in update profile api

---
+++ app/Http/Requests/UpdateProfileApiRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateProfileApiRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|max:50',
+            'last_name'=> 'required',
+            'phone' => 'required|max:50',
+            'gender'=> 'required',
+        ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'success'   => false,
+            'message'   => 'Validation errors',
+            'data'      => $validator->errors()
+        ]));
+    }
+
+    // public function messages() //OPTIONAL
+    // {
+    //     return [
+    //         'email.required' => 'Email is required',
+    //         'email.email' => 'Email is not correct'
+    //     ];
+    // }
+}
